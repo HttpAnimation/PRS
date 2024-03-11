f1a748730e1428ebef8f9ab7b18aefe02476ca6f
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2024-03-10 20:37:34 -0700

    Update gitter.bash

--- gitter.bash
+++ gitter.bash
@@ -1,15 +1,15 @@
 # Put gitter here
-mkdir NAME
-cd NAME
-git clone -b stable https://github.com/HttpAnimation/NAME.git
-mv NAME stable
-git clone -b blank https://github.com/HttpAnimation/NAME.git
-mv NAME blank 
-git clone -b extra https://github.com/HttpAnimation/NAME.git
-mv NAME extra
-git clone -b gh-pages https://github.com/HttpAnimation/NAME.git
-mv NAME gh-pages
-git clone -b gitter https://github.com/HttpAnimation/NAME.git
-mv NAME gitter
-git clone -b main https://github.com/HttpAnimation/NAME.git
-mv NAME main
+mkdir PRS
+cd PRS
+git clone -b stable https://github.com/HttpAnimation/PRS.git
+mv PRS stable
+git clone -b blank https://github.com/HttpAnimation/PRS.git
+mv PRS blank 
+git clone -b extra https://github.com/HttpAnimation/PRS.git
+mv PRS extra
+git clone -b gh-pages https://github.com/HttpAnimation/PRS.git
+mv PRS gh-pages
+git clone -b gitter https://github.com/HttpAnimation/PRS.git
+mv PRS gitter
+git clone -b main https://github.com/HttpAnimation/PRS.git
+mv PRS main
