e065c6f957f22507126f9a17d171aec1aba98b8b
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-10 20:51:16 -0700

    real versions

--- compile.sh
+++ compile.sh
@@ -1,4 +1,21 @@
-gcc server.c -o Builds/server
-gcc client.c -o Builds/client
+#!/bin/bash
 
-echo "$(date +'%Y-%m-%d %H:%M:%S')" > Builds/Version.ini
+# Create Builds folder if it doesn't exist
+mkdir -p Builds
+
+# Get current date and time
+current_datetime=$(date +'%Y-%m-%d_%H-%M-%S')
+
+# Create a folder with the current date and time
+build_folder="Builds/$current_datetime"
+mkdir -p "$build_folder"
+
+# Compile server.c and client.c into the build folder
+gcc server.c -o "$build_folder/server"
+gcc client.c -o "$build_folder/client"
+
+# Create a Version.ini file with the current date and time
+echo "$current_datetime" > "$build_folder/Version.ini"
+
+# Print the path to the build folder
+echo "Builds created at: $build_folder"
